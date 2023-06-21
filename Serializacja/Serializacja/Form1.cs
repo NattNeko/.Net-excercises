eb1df90c9d598c5ef3e2820f1cf2d28213be098b
Natalia Nagórka <80109525+NattNeko@users.noreply.github.com>
2023-06-21 15:33:53 +0200

    Add files via upload

---
+++ Serializacja/Serializacja/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Serializacja
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var employee = new Employee()
+            {
+                Name = "Jan",
+                ID = 1
+
+            };
+            string employeeSerialized = JsonConvert.SerializeObject(employee);
+            File.WriteAllText(@"C:\JS\Serialized.json", employeeSerialized);
+        }
+        class Employee
+        {
+            public string Name { get; set; }
+            public int ID { get; set; }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            string employeeSerialized = File.ReadAllText(@"C:\JS\Serialized.json");
+            Employee employee = JsonConvert.DeserializeObject<Employee>(employeeSerialized);
+         // textBox1 = employee.Name + employee.ID;
+        }
+
+       
+    }
+}
---
+++ Szyfrowanie/Szyfrowanie/Szyfrowanie/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szyfrowanie
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ROT13(textBox2.Text);
+        }
+
+        
+        public string ROT13(string input)
+        {
+            StringBuilder wynik = new StringBuilder();
+            Regex regex = new Regex("[A-Za-z]");
+            foreach (char c in input)
+            {
+                if (regex.IsMatch(c.ToString()))
+                {
+                    int kod = ((c & 223) - 52) % 26 + (c & 32) + 65;
+                    wynik.Append((char)kod);
+                }
+                else
+                    wynik.Append(c);
+            }
+            return wynik.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ROT13(textBox1.Text);
+        }
+
+       
+    }
+}
